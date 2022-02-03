538711828cd3d40ac9e20a87d0aa0bf3ca8fb418
Osvaldo Costa <osvaldo.costajr@gmail.com>
2022-02-03 15:06:21 -0300

    FavIcon and site information setted

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 export default {
-  url: 'https://strapi-landing-pages-project-2.herokuapp.com',
-  siteName: 'Otávio Miranda',
+  url: '',
+  siteName: 'Osvaldo Costa Next',
   defaultSlug: 'landing-page',
 };
